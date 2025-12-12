9e07a1b61be3a818071ed60240ee703753e98c6d
lucaszanfa <lucasfzanfa@gmail.com>
2025-12-12 18:34:31 -0300

    adicionando o arquivo

---
+++ extract_rom.py
@@ -0,0 +1,5 @@
+﻿with open('SAP_MELHORADO.circ', 'r', encoding='utf-8') as f:
+    data = f.read()
+start = data.index('<lib desc="#Memory" name="4">')
+end = data.index('</lib>', start) + len('</lib>')
+print(data[start:end])
